4811f8af8f824b8d639755194fb209759da591ec
Briniel <lord.devol@gmail.com>
2018-03-01 14:58:59 +0400

    Яндекс проверка готова.

--- ForFatherAutotest/src/test/java/BasicCheck/RadioTest.java
+++ ForFatherAutotest/src/test/java/BasicCheck/RadioTest.java
@@ -1,40 +1,33 @@
 package BasicCheck;
 
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 
 import java.util.concurrent.TimeUnit;
 
 import static BackgroundClasses.CheckingOS.CheckingMyOS;
 import static BackgroundClasses.ElementsScreens.*;
 
-public class YandexTest {
+public class RadioTest {
 
     @BeforeClass
-    public static void setup(){
+    public static void tolls(){
         System.setProperty("webdriver.chrome.driver", CheckingMyOS());
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("https://yandex.ru/");
+        driver.get("http://player.rockfm.ru/");
     }
 
 
     @Test
-    public void Login(){
-        Search().sendKeys("Selenium");
-        ButtonSearch().click();
-        int resul = ResultSearch().getSize().height;
-        if (resul == 0){
-            System.out.print("По запросу ничего не найдено");
-            Assert.assertFalse(true);
-        } else {
-            System.out.println("По запросу найдено "+ResultSearch().getSize().height);
-        }
+    public void Tell(){
+        CharchButton().click();
+        System.out.println(Stick().findElements(By.tagName("li")));
     }
-
-
 }
